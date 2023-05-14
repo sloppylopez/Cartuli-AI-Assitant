5c36252959025f02107309df33cdf57cb4388b09
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-05-14 23:21:20 +0200

    fix: Big refactor, add moar docu

---
+++ scripts/python/ears/voice_command_listener_threaded.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import sys
+
+# NOTE: this example requires PyAudio because it uses the Microphone class
+import keyboard
+import time
+from threading import Thread
+from queue import Queue
+
+import speech_recognition as sr
+
+from scripts.python.mouth.cartuli_says import cartuli_says
+
+r = sr.Recognizer()
+audio_queue = Queue()
+
+
+def recognize_worker():
+    # this runs in a background thread
+    while True:
+        audio = audio_queue.get()  # retrieve the next audio processing job from the main thread
+        if audio is None:
+            break  # stop processing if the main thread is done
+        # received audio data, now we'll recognize it using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        audio_queue.task_done()  # mark the audio processing job as completed in the queue
+
+
+# start a new thread to recognize audio, while this thread focuses on listening
+recognize_thread = Thread(target=recognize_worker)
+recognize_thread.daemon = True
+recognize_thread.start()
+
+with sr.Microphone() as source:
+    is_f19_pressed = False
+    try:
+        while True:
+            if keyboard.is_pressed('f19'):
+
+                cartuli_says('Say something, I\'m all ears...')
+                if not is_f19_pressed:
+                    audio_queue.put(r.listen(source))
+                    is_f19_pressed = True
+            else:
+                is_f19_pressed = False
+            time.sleep(0.1)  # Add a small delay to reduce CPU usage
+    except KeyboardInterrupt:
+        pass
+
--- scripts/python/main.py
+++ scripts/python/main.py
@@ -1,10 +1,13 @@
 from scripts.python.ears.voice_command_listener import voice_command_listener
+from scripts.python.ears.voice_command_listener_threaded import recognize_worker
 from scripts.python.mouth.cartuli_says import cartuli_says
 
 
 def main():
     cartuli_says("¡Cómo están los máquinas!")
-    voice_command_listener()
+    recognize_worker()
+    cartuli_says("Program finished.")
+    # voice_command_listener()
 
 if __name__ == "__main__":
     main()
