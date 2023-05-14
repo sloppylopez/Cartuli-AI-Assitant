057466412a6ab75eebe1fedb538e06d0ec4814b7
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-05-15 00:31:07 +0200

    feat: It's alive!!

--- scripts/python/main.py
+++ scripts/python/main.py
@@ -1,4 +1,4 @@
-from scripts.python.ears.voice_command_listener import voice_command_listener
+# from scripts.python.ears.voice_command_listener import voice_command_listener
 from scripts.python.ears.voice_command_listener_threaded import recognize_worker
 from scripts.python.mouth.cartuli_says import cartuli_says
 
@@ -8,6 +8,7 @@ def main():
     recognize_worker()
     cartuli_says("Program finished.")
     # voice_command_listener()
+
 
 if __name__ == "__main__":
     main()
