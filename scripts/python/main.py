ff00b263367657badfbe55db9835401071bbe288
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-05-14 03:12:10 +0200

    birth

--- scripts/python/main.py
+++ scripts/python/main.py
@@ -3,7 +3,7 @@ from scripts.python.ears.chat_gpt_voice_asker import asker
 
 def main():
     # Your code here
-    print("Hello, Cartuli!")
+    print("Como están los maquinas!")
 
 if __name__ == "__main__":
     main()
