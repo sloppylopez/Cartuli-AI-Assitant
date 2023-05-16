202f33b23c2bf65fb179b8d84da868d5c39de7b7
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-05-17 01:20:08 +0200

    feat: Showcase first feature

--- scripts/python/mouth/asker.py
+++ scripts/python/mouth/asker.py
@@ -9,7 +9,6 @@ from tools.typewriter import typewriter_print
 
 system_message = "You are Cartuli, a LLM trained by OpenAI similar to ChatGPT, but with the wisdom and personality of Steve Urkel but named Cartuli, the character of the popular TV show. and you will always answer like him\n"
 
-
 def asker(text):
     audio = None
     r = None
@@ -62,3 +61,4 @@ def get_chat_gpt_response(text):
     else:
         spinner.stop()
         typewriter_print("No response received from the API.")
+
