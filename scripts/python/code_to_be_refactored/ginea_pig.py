43f00d769206d31e1f3505527376f8f41e8e6ca0
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-06-07 22:09:21 +0200

    :dummy: Commit to test code in local

--- scripts/python/code_to_be_refactored/ginea_pig.py
+++ scripts/python/code_to_be_refactored/ginea_pig.py
@@ -1,13 +1,25 @@
-def calculate_average2(numbers):
-    sum_ = 0
-    count = 0
-    for i in range(len(numbers)):
-        sum_ += numbers[i]
-        count += 1
-    average = sum_ / count
-    return average
+import math
 
-# Test the function
-nums = [2, 4, 6, 8, 10]
-result = calculate_average2(nums)
-print("The average is: " + str(result))
+
+def calculate_area(radius):
+    # Flaw 1: Inconsistent naming convention (should use snake_case)
+    Result = 3.14 * radius * radius
+
+    # Flaw 2: Redundant math module import (math module not required for simple multiplication)
+    area = math.pi * radius ** 2
+
+    # Flaw 3: Hard-coded constant value (should use math.pi instead of 3.14)
+    radius_squared = radius * radius
+    circle_area = math.pi * radius_squared
+
+    # Flaw 4: Unused variable (Result is assigned but never used)
+    return circle_area
+
+
+# Flaw 5: Missing function call with an argument
+calculate_area()
+
+# Flaw 6: Incorrect parameter type (expects a number, not a string)
+calculate_area("5")
+
+# Flaw 7: No type checking or error handling for invalid input
