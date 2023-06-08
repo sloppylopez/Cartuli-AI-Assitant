21d48606a77a8063a5e179f1fb067a7d335e1c85
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-06-08 22:36:44 +0200

    commit to test code

--- scripts/python/code_to_be_refactored/ginea_pig.py
+++ scripts/python/code_to_be_refactored/ginea_pig.py
@@ -1,66 +1,25 @@
-import hashlib
-import json
-import os
-
-from eyes.detect_line_separators import detect_line_separator
-from hands.reformat_2_pep8 import replace_file_lf_with_crlf
-from tools.logger import logger
+import math
 
 
-def read_json_file(file_path):
-    data = {}
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-    except Exception as e:
-        logger(f"Could not read file {file_path}; {e}")
-        pass
-    return data
+def calculate_area(radius):
+    # Flaw 1: Inconsistent naming convention (should use snake_case)
+    Result = 3.14 * radius * radius
+
+    # Flaw 2: Redundant math module import (math module not required for simple multiplication)
+    area = math.pi * radius ** 2
+
+    # Flaw 3: Hard-coded constant value (should use math.pi instead of 3.14)
+    radius_squared = radius * radius
+    circle_area = math.pi * radius_squared
+
+    # Flaw 4: Unused variable (Result is assigned but never used)
+    return circle_area
 
 
-def read_files_and_hash(folder_path):
-    # Check if folder_path is a file
-    if os.path.isfile(folder_path):
-        with open(folder_path, 'rb') as file_to_refactor:
-            if detect_line_separator(file_to_refactor) == 'LF':
-                replace_file_lf_with_crlf(file_to_refactor)
-            file_content = file_to_refactor.read()
-            file_hash = hashlib.sha256(file_content).hexdigest()
-            file_contents = {file_hash: (file_content.decode(), os.path.basename(folder_path))}
-    else:
-        # Read all files in the given folder
-        file_contents = {}
-        for file_name in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, file_name)
-            if os.path.isfile(file_path):
-                if detect_line_separator(file_path) == 'LF':
-                    replace_file_lf_with_crlf(file_path)
-                with open(file_path, 'rb') as file_to_refactor:
-                    file_content = file_to_refactor.read()
-                    file_hash = hashlib.sha256(file_content).hexdigest()
-                    file_contents[file_hash] = (file_content.decode(), file_name)
-    return file_contents
+# Flaw 5: Missing function call with an argument
+calculate_area()
 
+# Flaw 6: Incorrect parameter type (expects a number, not a string)
+calculate_area("5")
 
-def read_file_binary(file_path):
-    # Check if folder_path is a file
-    if os.path.isfile(file_path):
-        with open(file_path, 'rb') as file_to_read:
-            file_content = file_to_read.read()
-    return file_content
-
-
-def read_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            file_contents = file.read()
-        return file_contents
-    except IOError:
-        print(f"Error: Could not read file '{file_path}'")
-
-
-if __name__ == "__main__":
-    # Example usage:
-    json_file_path = 'C:/Users/sergi/PycharmProjects/Cartuli-AI-Assitant/scripts/python/.cartuli/long_term_hash_memory.json'
-    json_data = read_json_file(json_file_path)
-    print(list(json_data.keys())[0])
+# Flaw 7: No type checking or error handling for invalid input
