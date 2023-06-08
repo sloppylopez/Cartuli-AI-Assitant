d22e727cb717588960d528a1c8498d1bd4c68614
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-06-08 22:59:28 +0200

    :bug: AI rites code that does not compile

--- scripts/python/code_to_be_refactored/ginea_pig.py
+++ scripts/python/code_to_be_refactored/ginea_pig.py
@@ -1,14 +1,6 @@
 import math
 
 def calculate_area(radius: float) -> float:
-    """Calculate the area of a circle given its radius.
-    
-    Args:
-        radius (float): The radius of the circle.
-    
-    Returns:
-        float: The area of the circle.
-    """
     area = math.pi * radius ** 2
     return area
 
