3f69f99ec316f51b7e835abe5d3beab4a35b273a
Sergio Lopez <sergio.lopez.belinchon@es.ey.com>
2023-06-08 23:01:46 +0200

    :bug: AI rites code that does not compile

--- scripts/python/code_to_be_refactored/ginea_pig.py
+++ scripts/python/code_to_be_refactored/ginea_pig.py
@@ -1,22 +1,7 @@
 import math
 
-
 def calculate_area(radius):
-    # Flaw: Inconsistent naming convention (should use snake_case)
-    Result = 3.14 * radius * radius
+    result = math.pi * radius ** 2
+    return result
 
-    # Flaw: Redundant math module import (math module not required for simple multiplication)
-    area = math.pi * radius ** 2
-
-    # Flaw: Hard-coded constant value (should use math.pi instead of 3.14)
-    radius_squared = radius * radius
-    circle_area = math.pi * radius_squared
-
-    # Flaw: Unused variable (Result is assigned but never used)
-    return circle_area
-
-
-# Flaw: Incorrect parameter type (expects a number, not a string)
-calculate_area("5")
-
-# Flaw: No type checking or error handling for invalid input
+calculate_area(5)
